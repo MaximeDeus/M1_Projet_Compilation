3747887612df191cbcbe30d916a69d2f75213e9f
MaximeDeus <maxime.deroissart@etu.univ-grenoble-alpes.fr>
2018-12-12 11:36:32 +0100

    Découpage du fichier Type.java, création du package type (contient interface Type + les différents types)

---
+++ ocamlCompiler/ocamlCompiler/src/main/java/okalm/ocamlcompiler/java/type/TArray.java
@@ -0,0 +1,5 @@
+package okalm.ocamlcompiler.java.type;
+
+public class TArray extends Type {
+    //TODO
+}
---
+++ ocamlCompiler/ocamlCompiler/src/main/java/okalm/ocamlcompiler/java/type/TUnit.java
@@ -0,0 +1,5 @@
+package okalm.ocamlcompiler.java.type;
+
+public class TUnit extends Type {
+    //TODO
+}
