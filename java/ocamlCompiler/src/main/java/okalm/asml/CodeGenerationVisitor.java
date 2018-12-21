870cb6ccdc33ffaaef4f124365d4d92f2c8ceeb5
geoliak <geoliak@users.noreply.github.com>
2018-12-21 13:37:33 +0100

    Genération de code, il faut tester prochainement les add et les sub pour des bugs

--- java/ocamlCompiler/src/main/java/okalm/asml/CodeGenerationVisitor.java
+++ java/ocamlCompiler/src/main/java/okalm/asml/CodeGenerationVisitor.java
@@ -32,81 +32,101 @@ public class CodeGenerationVisitor implements AsmlErrorVisitor<String> {
     }
 
     @Override
-    public String visit(Add e) throws Exception {
+    public String visit(Ident e) throws Exception {
+        return e.ident;
+    }
 
+    @Override
+    public String visit(Add e) throws Exception {
         return (e.ident.accept(this) + "," + e.ioi.accept(this));
     }
 
     @Override
     public String visit(Sub e) throws Exception {
         return (e.ident.accept(this) + ", " + e.ioi.accept(this));
-
     }
 
     @Override
     public String visit(Asmt e) throws Exception {
         if (e.ident != null) {
+            String[] regs;
             switch (e.e.getClass().toString()) {
 
                 case "Add":
+                    regs = choixRegistres(e.ident.toString() + "," + e.e.accept(this));
+                    writer.write("ADD" + regs[0] + ", " + regs[1] + ", " + regs[2] + "\n");
+                    break;
+                case "Sub":
+                    regs = choixRegistres(e.ident.toString() + "," + e.e.accept(this));
+                    writer.write("SUB " + regs[0] + ", " + regs[1] + ", " + regs[2] + "\n");
+                    break;
+                case "":
+                default:
 
-                    String[] regs = findRegisters(e.ident.toString() + "," + e.e.accept(this));
+                    break;
 
-                    writer.write("ADD" + regs[0] + regs[1] + regs[2]);
             }
+
         }
         return null;
     }
 
-    @Override
-    public String visit(Ident e) throws Exception {
-        return e.ident;
-    }
-
-    //Prend trois variables et retourne les registres pour une instruction à trois adresses
-    private String[] findRegisters(String e) throws Exception {
+    private String[] choixRegistres(String e) throws Exception {
         String[] args = e.split(",");
-        String[] ret = new String[3];
-        for (int i = 0; i < 3; i++) {
+        String[] ret = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
             int r = 0;
             while (r < 13 && ret[i] == null) {
-                if (args[i].equals(registres[r])) {
+                if (args[i].equals(registres[r])) {//déja contenu dans un registre
                     ret[i] = "r" + r;
 
                 } else if (registres[r] == null) {//Pas déja dans un registre, alors on cherche pour un registre vide
                     ret[i] = "r" + r;
+                    remplirRegistre(r, args[i]);
                 } else if (!(variablesActives.contains(registres[r]))) {//si la variable contenue dans registre r n'est pas utilisée plus tard
                     ret[i] = "r" + r;
+                    remplirRegistre(r, args[i]);
                 }
                 r++;
             }
             r = 0;
             while (r < 13 && ret[i] == null) {
                 if (variablesActives.contains(registres[r])) {//si la variable contenue dans registre r est utilisée plus tard, mais on n'a plus d'options
-                    sauvegardeMemoire(r, registres[r]);
-
-                    frameCounter++;
-
-                    //                    writer.write("LDR " + "r"+r+""
+                    sauvegardeMemoire(r);
+                    remplirRegistre(r, args[i]);
                     ret[i] = "r" + r;
                 }
                 r++;
+            }
+            try {                                   //Si le dernier argument est un int, alors on le parse et on le met dans un registre déjà decidé par la fonction
+                int entier = Integer.parseInt(args[args.length - 1]);
+                writer.write("MOV " + ret[args.length - 1] + ", " + "#" + entier);
+            } catch (NumberFormatException nfe) {
             }
             //TODO Ajouter le cas ou si un des deux arguments n'est pas actif dans la suite, on peut mettre le resultat dans son reg ex.: x=y+z -> R0=R0+R1 si y pas actif dans la suite
         }
         return args;
     }
 
-    public void sauvegardeMemoire(int r, String id) throws IOException {
-        writer.write("STR " + "r" + r + "[fp+" + (frameCounter * 4) + "]"); //On sauvegarde la variable qui etait dans le registre à utiliser dans la memoire
-        ArrayList<String> liste = mem.get("registres[r]");
+    public void sauvegardeMemoire(int r) throws IOException {
+        writer.write("STR " + "r" + r + "[fp+" + (frameCounter * 4) + "]" + "\n"); //On sauvegarde la variable qui etait dans le registre à utiliser dans la memoire...
+        ArrayList<String> liste = mem.get(registres[r]);
 
         if (liste == null) {
-            liste = new ArrayList<String>();
-
+            liste = new ArrayList<>();
+            liste.add("[fp+" + (frameCounter * 4) + "]");           //...et on garde une trace de ce fait
+            mem.put(registres[r], liste);
         }
-//                    liste.add("[fp+" + (frameCounter * 4) + "]");           //Et on garde une trace de ce fait
+        frameCounter++;
+    }
 
+    private void remplirRegistre(int r, String arg) throws Exception {
+        ArrayList<String> liste = mem.get(arg);
+        if (liste == null) {
+            throw new Exception("Variable not found in memory, sorry");
+        } else {
+            writer.write("LDR " + "r" + r + liste.get(liste.size() - 1) + "\n"); //Recuperer la sauvegarde la plus recente, idealement il y a q'une de toute façon
+        }
     }
 
     @Override
@@ -161,7 +181,7 @@ public class CodeGenerationVisitor implements AsmlErrorVisitor<String> {
 
     @Override
     public String visit(Int e) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Integer.toString(e.e);
     }
 
     @Override
