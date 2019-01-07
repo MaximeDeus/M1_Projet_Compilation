b7d48c66dd19f1e6a19eb33e1040cd0eb6c037f9
RonanDefours <ronan.defours@etu.univ-grenoble-alpes.fr>
2019-01-07 14:38:08 +0100

    modif worklist

--- java/ocamlCompiler/src/main/java/okalm/asml/WorkList.java
+++ java/ocamlCompiler/src/main/java/okalm/asml/WorkList.java
@@ -13,27 +13,16 @@ import java.util.List;
  * @author defoursr
  */
 public class WorkList {
-    public Exp_asml prec,suc,exp; //précédent, successeur, expression
+    public Exp_asml exp;
+    public List<Exp_asml> prec,suc; //précédent, successeur 
     public List<String> gen,kill;
 
     
-    public WorkList(Exp_asml exp) {
+
+     public WorkList(Exp_asml exp) {
         this.exp = exp;
-        this.gen = new ArrayList();
-        this.kill = new ArrayList();
-    }
-    
-    public WorkList(Exp_asml exp,Exp_asml prec) {
-        this.exp = exp;
-        this.prec = prec;
-        this.gen = new ArrayList();
-        this.kill = new ArrayList();
-    }
-    
-     public WorkList(Exp_asml exp,Exp_asml prec, Exp_asml suc) {
-        this.exp = exp;
-        this.prec = prec;
-        this.suc = suc;
+        this.prec = new ArrayList();
+        this.suc = new ArrayList();
         this.gen = new ArrayList();
         this.kill = new ArrayList();
     }
