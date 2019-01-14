f22e8ad764f58bdb547d50adbac0d8e686fb781b
jules <jules.diard@gmail.com>
2019-01-14 18:02:18 +0100

    JavaDoc de la classe TypeChecking

--- java/ocamlCompiler/src/main/java/okalm/typechecking/TypeVisitor.java
+++ java/ocamlCompiler/src/main/java/okalm/typechecking/TypeVisitor.java
@@ -12,6 +12,7 @@ import java.util.*;
  */
 public class TypeVisitor implements ObjErrorVisitor<Type> {
 
+
     private Map<String, Type> listeVar;
     private Map<String, Type> listeFun;
     private Map<String, Type> listeArray;
@@ -19,7 +20,10 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
     private Map<Exp, Type> listeExp;
     private Map<Exp, List<Type>> listeExpTuple;
     private int num_let;
-
+    /**
+     * 
+     * @param n pour savoir a quel let on est (toujours égal à zéro)
+     */
     public TypeVisitor(int n) {
         listeVar = new HashMap();
         listeFun = new HashMap();
@@ -42,12 +46,12 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
     }
 
     /**
-     * Ajoute un Id à l'environnement l actuel
+     * Ajout d'un Id à l'environnement actuel
      *
-     * @param l  name of the map
-     * @param id new Id to add to the environement
-     * @param t  type to associate with the Id
-     * @throws TypeException if the Id has already been declared before
+     * @param l nom de la map
+     * @param id nouvel identifiant a ajouté à la map
+     * @param t  type à associer avec l'identifiant
+     * @throws TypeException si l'identifiant est déjà utilisé
      */
     private void addId(Map<String, Type> l, Id id, Type t) throws TypeException {
         if (l == listeFun && l.containsKey(id.id)) {
@@ -59,52 +63,106 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
             l.put(id.id, t);
         }
     }
-
+    /**
+     * Ajout d'une expression à l'environnement d'expression actuel
+     * 
+     * @param exp expression concernée, identifiée par sa référence
+     * @param t type associé à l'expression
+     */
     private void addExpType(Exp exp, Type t) {
         listeExp.put(exp, t);
     }
-
+    
+    /**
+     * Ajout à la liste de type représentant les éléments d'un tuple et fait la liaison avec l'expression (non utilisé et non utilisable)
+     * 
+     * @param exp expression concernée, identifiée par son adresse
+     * @param t liste de type associé à l'expression
+     */
     private void addExpTuple(Exp exp, ArrayList<Type> t) {
         listeExpTuple.put(exp, (List) t);
     }
-
+    /**
+     * Compare un type avec une classe, renvoie true si elles sont égales
+     * 
+     * @param t type 
+     * @param s classe (TInt, TBool, etc)
+     * @return boolean
+     */
     private Boolean testType(Type t, Class s) {
         return t.getClass().getSimpleName().equals(s.getSimpleName());
     }
-
+    
+    /**
+     * Visite une feuille Unit et renvoie un Type Tunit
+     * 
+     * @param e expression étudiée
+     * @return TUnit
+     */
     @Override
     public Type visit(Unit e) {
         return new TUnit();
     }
-
+    
+    /**
+     * Visite une feuille Bool et renvoie un Type TBool
+     * 
+     * @param e expression étudiée
+     * @return TBool
+     */
     @Override
     public Type visit(Bool e) {
         return new TBool();
     }
-
+    
+    /**
+     * Visite une feuille Int et renvoie un Type TInt
+     * 
+     * @param e expression étudiée
+     * @return TInt
+     */
     @Override
     public Type visit(Int e) {
         return new TInt();
     }
 
-
+    /**
+     * Visite une feuille Float et renvoie un Type TFloat
+     * 
+     * @param e expression étudiée
+     * @return TFloat
+     */
     @Override
     public Type visit(okalm.ast.Float e) {
         return new TFloat();
     }
-
+    
+    /**
+     * Visite un nœud Not et vérifie que son fils renvoie bien un type TBool. Renvoie un type TBool.
+     * 
+     * @param e expression étudiée
+     * @return TBool
+     * @throws Exception si son fils n'est pas un TBool
+     */
     @Override
     public Type visit(Not e) throws Exception {
         Type t = e.e.accept(this);
         if (this.testType(t, TUndef.class)) {
             return new TBool();
         }
-        if (!testType(t, TBool.class)) { //si le type du fils est différent du type entre guillements
+        if (!testType(t, TBool.class)) {
             throw new TypeException("Bad typing at Not: boolean expected");
         }
         return new TBool();
     }
-
+    
+    /**
+     * Visite un nœud Neg et vérifie que le fils renvoie bien un type TInt. Renvoie un type TInt.
+     * 
+     * @param e expression étudiée
+     * @return TInt
+     * @throws Exception si son fils n'est pas un TInt
+     */
     @Override
     public Type visit(Neg e) throws Exception {
         Type t = e.e.accept(this);
@@ -116,12 +174,19 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
             }
             return new TInt();
         }
-        if (!testType(t, TInt.class)) { //si le type du fils est différent du type entre guillements
+        if (!testType(t, TInt.class)) {
             throw new TypeException("Bad typing at Neg: Integer expected");
         }
         return new TInt();
     }
 
+    /**
+     * Visite un nœud Add, vérifie si ses deux fils renvoient des types Tint. Renvoie un Type Tint.
+     * 
+     * @param e expression étudiée
+     * @return TInt
+     * @throws Exception si son fils n'est pas un TInt
+     */
     @Override
     public Type visit(Add e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -145,7 +210,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
         return new TInt();
     }
-
+    /**
+     * Visite un nœud Sub, vérifie si ses deux fils sont de type Tint. Renvoie un Type Tint
+     * 
+     * @param e expression étudiée
+     * @return TInt
+     * @throws Exception si son fils n'est pas un TInt
+     */
     @Override
     public Type visit(Sub e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -162,7 +233,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
         return new TInt();
     }
-
+    /**
+     * Visite un nœud FNeg, vérifie si son fils est de type TFloat
+     * 
+     * @param e expression étudiée
+     * @return TFloat
+     * @throws Exception si son fils n'est pas un TInt
+     */
     @Override
     public Type visit(FNeg e) throws Exception {
         Type t = e.e.accept(this);
@@ -175,6 +252,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return new TFloat();
     }
 
+     /**
+     * Visite un nœud Sub, vérifie si ses deux fils sont Type TFloat. Renvoie un Type TFloat
+     * 
+     * @param e expression étudiée
+     * @return TFloat
+     * @throws Exception si ses fils ne sont pas du type TFloat
+     */
     @Override
     public Type visit(FAdd e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -191,7 +275,14 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
         return new TFloat();
     }
-
+    
+     /**
+     * Visite un nœud FSub, vérifie si ses deux fils sont Type TFloat. Renvoie un Type TFloat
+     * 
+     * @param e expression étudiée
+     * @return TFloat
+     * @throws Exception  si ses fils ne sont pas du type TFloat
+     */
     @Override
     public Type visit(FSub e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -210,6 +301,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return new TFloat();
     }
 
+     /**
+     * Visite un nœud FMul, vérifie si ses deux fils sont de Type TFloat ou TInt. Renvoie un Type TFloat
+     * 
+     * @param e expression étudiée
+     * @return TFloat
+     * @throws Exception si ses fils ne sont pas du type TFloat ou TInt
+     */
     @Override
     public Type visit(FMul e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -232,6 +330,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return new TFloat();
     }
 
+     /**
+     * Visite un nœud FDiv, vérifie si ses deux fils sont Type TFloat ou TInt. Renvoie un Type TFloat
+     * 
+     * @param e expression étudiée
+     * @return TFloat
+     * @throws Exception si ses fils ne sont pas de type TFloat ou TInt
+     */
     @Override
     public Type visit(FDiv e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -249,6 +354,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return new TFloat();
     }
 
+    /**
+     * Visite un nœud Eq, vérifie si ses deux fils sont du même Type et renvoie un TBool
+     * 
+     * @param e expression étudiée
+     * @return TBool
+     * @throws Exception si ses fils ne sont pas du même type
+     */
     @Override
     public Type visit(Eq e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -264,7 +376,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
         return new TBool();
     }
-
+    /**
+     * Visite un nœud LE, vérifie si ses deux fils sont du même Type et renvoie un TBool
+     * 
+     * @param e expression étudiée
+     * @return TBool
+     * @throws Exception si ses fils ne sont pas du même type
+     */
     @Override
     public Type visit(LE e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -281,7 +399,14 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
         return new TBool();
     }
-
+    
+    /**
+     * Visite un noeud If, vérifie si la première partie est une expression booléenne et que les deux autres fils sont du même type. Renvoie ce dernier.
+     * 
+     * @param e expression étudiée
+     * @return le type de ses deux fils
+     * @throws Exception si le premier fils n'est pas une expression booléenne ou si ses autres fils ne sont pas du même type.
+     */
     @Override
     public Type visit(If e) throws Exception {
         Type t1 = e.e1.accept(this);
@@ -304,6 +429,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return t2;
     }
 
+    /**
+     * Visite un noeud let, vérifie si le nom de la variable existe déjà dans l'environnement de variable et renvoie le type de cette dernière. Si c'est le premier let la méthode renvoie TUnit.
+     * 
+     * @param e expression étudiée
+     * @return type de la variable
+     * @throws Exception si le nom de la variable existe déjà
+     */
     @Override
     public Type visit(Let e) throws Exception {
         num_let++;
@@ -324,11 +456,18 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return t2;
     }
 
+    /**
+     * Visite un noeud Var, vérifie si la variable existe dans l'environnement de variable. Renvoie son type.
+     * 
+     * @param e expression étudiée
+     * @return le type de la variable
+     * @throws Exception si le nom de la variable n'existe pas
+     */
     @Override
     public Type visit(Var e) throws Exception {
 
         if (!listeVar.containsKey(e.id.id) && !listeArray.containsKey(e.id.id) && !listeTuple.containsKey(e.id.id)) {
-            throw new TypeException("Unknow variable " + e.id.id);
+            throw new TypeException("Unknown variable " + e.id.id);
         }
         if (listeVar.containsKey(e.id.id)) {
             return listeVar.get(e.id.id);
@@ -341,6 +480,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
     }
 
+    /**
+     * Visite un noeud LetRec, vérifie si la fonction existe déjà dans l'environnement de fonction et renvoie le type de retour de la fonction.
+     * 
+     * @param e expression étudiée
+     * @return le type de retour de la fonction
+     * @throws Exception si le nom de fonction existe déjà
+     */
     @Override
     public Type visit(LetRec e) throws Exception {
         num_let++;
@@ -372,6 +518,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         }
     }
 
+    /**
+     * Visite un noeud App, vérifie que le nom de la fonction appelée existe dans l'environnement de fonction. Renvoie le type de retour de la fonction
+     * 
+     * @param e expression étudiée
+     * @return type de retour de la fonction
+     * @throws Exception si la fonction n'existe pas
+     */
     @Override
     public Type visit(App e) throws Exception {
         VarVisitor vv = new VarVisitor();
@@ -391,6 +544,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return listeFun.get(rendu);
     }
 
+    /**
+     * Visite un noeud Tuple, et ajoute le tuple dans l'environnement de Tuple (méthode non finie et non utilisée)
+     * 
+     * @param e expression étudiée
+     * @return type TTuple
+     * @throws Exception si on veut ajouter des limites pour les types autorisés dans les tuples 
+     */
     @Override
     public Type visit(Tuple e) throws Exception {
         ArrayList<Type> lt = new ArrayList();
@@ -403,12 +563,26 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return new TTuple();
     }
 
+    /**
+     * Visite un noeud LetTuple, vérifie que la découpe du tuple est correct est déplace les éléments dans l'environnement de variable. Renvoie le type TTuple (pas implémenté)
+     * 
+     * @param e expression étudiée
+     * @return TTuple
+     * @throws Exception si la découpe du tuple est mal faite
+     */
     @Override
     public Type visit(LetTuple e) throws Exception {
         //TODO
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Visite un noeud Array, vérifie que la fonction est appelée avec les arguments bien typés et si les données insérées sont de type TInt ou TFloat 
+     * 
+     * @param e expression étudiée
+     * @return type TArray
+     * @throws Exception si le premier fils est bien un entier et si les données insérées sont des TInt ou TFloat
+     */
     @Override
     public Type visit(Array e) throws Exception {
 
@@ -430,6 +604,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
 
     }
 
+    /**
+     * Visite un noeud Get, vérifie le type des arguments et renvoie le type des données du tableau
+     * 
+     * @param e expression étudiée
+     * @return type des données du tableau (TInt ou TFloat)
+     * @throws Exception si les arguments du noeud Get ne sont pas du bon type (le premier un TArray et le deuxième TInt)
+     */
     @Override
     public Type visit(Get e) throws Exception {
         String id = "error";
@@ -454,6 +635,13 @@ public class TypeVisitor implements ObjErrorVisitor<Type> {
         return listeArray.get(id);
     }
 
+    /**
+     * Visite un noeud Put, et vérifie si les valeurs que l'on veut insérer sont du même type que les données du tableau
+     * 
+     * @param e expression étudiée
+     * @return TUnit
+     * @throws Exception si l'élément à insérer n'est pas du même type que les élément du tableau 
+     */
     @Override
     public Type visit(Put e) throws Exception {
         String id = "error";
