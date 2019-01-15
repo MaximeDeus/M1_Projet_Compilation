d62e4d33df92ef55267ad4fe0649495f90af7156
jules <jules.diard@gmail.com>
2019-01-15 09:30:44 +0100

    merge

--- java/ocamlCompiler/src/main/java/okalm/typechecking/TypeException.java
+++ java/ocamlCompiler/src/main/java/okalm/typechecking/TypeException.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package okalm.typechecking;
 
 /**
- *
  * @author defoursr
  */
 public class TypeException extends Exception {
 
+
     public TypeException(){
         super("Type checking error");
     }
-
+    
     public TypeException(String msg) {
         super(msg);
     }
 }
+
+    
